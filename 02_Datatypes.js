6d0a7db8d1a746e97a6197671daa8dc8da85e413
rohitsirohi85 <rohitsirohi1012@gmail.com>
2025-02-02 16:32:55 +0530

    datatypes

---
+++ 02_Datatypes.js
@@ -0,0 +1,23 @@
+// there are 8 datatypes in js
+
+let x=23;  // integer/Number , range 2 to power 53
+let y="String" // string datatypes
+let z= true; // boolean
+// and many more like = 
+
+// bigInt  =>  for big number bigger than 2 to power 53
+// undefined => when a variable is undefined
+// null  =>  when want to return a null value
+// symbol => unique 
+// object => grp of properties
+
+// want to check datatype for a particular value
+console.log(typeof(true));   // return boolean
+
+console.log(typeof(y));   // coz y is storing a string value return string
+
+console.log(typeof(x));   // coz x is storing a number return number
+
+console.log(typeof(null));  // return null coz null is indirectly is an object 
+
+console.log(typeof(undefined)); // return undefined
